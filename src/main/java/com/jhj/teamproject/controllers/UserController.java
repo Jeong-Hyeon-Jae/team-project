7a73c69a242ecb5ac5a67013bcfbd362c32bfedd
yoonjunghyeon <90305884+yoonjunghyeon@users.noreply.github.com>
2025-05-07 17:32:01 +0900

    commit
    컨트롤러 수정

--- src/main/java/com/jhj/teamproject/controllers/UserController.java
+++ src/main/java/com/jhj/teamproject/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.jhj.teamproject.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping()
+@RequestMapping(value = "/user")
 public class UserController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String getLogin() {
+        return "/user/login";
+    }
 }
