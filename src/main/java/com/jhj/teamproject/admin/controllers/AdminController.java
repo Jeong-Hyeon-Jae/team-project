b53393197137377ce7574577acc4c3b6be89fb38
yoonjunghyeon <90305884+yoonjunghyeon@users.noreply.github.com>
2025-05-08 17:24:20 +0900

    admin.html ui
    UI구성

--- src/main/java/com/jhj/teamproject/admin/controllers/AdminController.java
+++ src/main/java/com/jhj/teamproject/admin/controllers/AdminController.java
@@ -1,4 +1,16 @@
 package com.jhj.teamproject.admin.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/")
 public class AdminController {
+
+    @RequestMapping(value = "/calendar", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String getCalendar() {
+        return "/admin/admin";
+    }
 }
