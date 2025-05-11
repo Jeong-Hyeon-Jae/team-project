f1037f2248a58d9c02a9a87ca2cdd347e2a9f02f
yoonjunghyeon <90305884+yoonjunghyeon@users.noreply.github.com>
2025-05-11 23:48:01 +0900

    list.html
    리스트 만듬

--- src/main/java/com/jhj/teamproject/admin/controllers/AdminController.java
+++ src/main/java/com/jhj/teamproject/admin/controllers/AdminController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/admin")
 public class AdminController {
 
-    @RequestMapping(value = "/calendar", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public String getCalendar() {
         return "/admin/admin";
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public String getList() {
+        return "/admin/list";
     }
 }
